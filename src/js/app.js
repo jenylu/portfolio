4d69da5f90d74e673e7ab0a47b6adeb9dd654243
Jennifer Lu <jenylu@fb.com>
2023-04-18 22:07:35 -0700

    updating grid and other pages

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+window.$ = require('jquery');
+window.layout = require('image-layout/layouts/fixed-partition');
+
+// var result = layout(elements, {
+//     align: 'center', // optional
+//     containerWidth: 800,
+//     idealElementHeight: 300,
+//     spacing: 0
+// });
+require('./main.js');
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/js/app.js',
+  mode: 'development',
+  output: {
+	path: `${__dirname}/dist`,
+	filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
